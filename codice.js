4e194458fcc8e862197b63668d8aaf1d4771dd2a
Nzo-ri <lor3nzo2005@gmail.com>
2025-10-29 09:49:38 +0100

    base_sito

--- codice.js
+++ codice.js
@@ -1,22 +1,9 @@
-// script.js
-const titolo = document.getElementById('titolo');
-const bottone = document.getElementById('bottone');
+function mostraSezione(id) {
+      // Nasconde tutte le sezioni
+      const sezioni = document.querySelectorAll("section");
+      sezioni.forEach(s => s.classList.remove("active"));
 
-fetch('./data.json')
-  .then(response => {
-    if (!response.ok) throw new Error("Errore caricamento dati: " + response.status);
-    return response.json();
-  })
-  .then(data => {
-    // applica colori / messaggi
-    document.body.style.backgroundColor = data.colorePrimario;
-    titolo.textContent = data.messaggioSaluto;
-
-    bottone.addEventListener('click', () => {
-      alert(data.dizionario.hello + "!");
-      titolo.textContent = data.dizionario.goodbye;
-    });
-  })
-  .catch(error => {
-    console.error("Errore:", error);
-  });
+      // Mostra solo la sezione selezionata
+      const sezioneAttiva = document.getElementById(id);
+      sezioneAttiva.classList.add("active");
+    }
